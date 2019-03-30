5723fa2527d32a20f1167d07708abc0687e62248
Ian sun <lauer3912@gmail.com>
2019-03-30 08:44:05 +0800

    将Lucy电脑加入到队列中

--- Python-Books/code-projects/auto-appium-android/libs/vmsmodify.py
+++ Python-Books/code-projects/auto-appium-android/libs/vmsmodify.py
@@ -65,7 +65,6 @@ class VMSModifyHandler(object):
                         one_config.hasAttribute('path') and \
                         one_config.hasAttribute('vmname'):
                     config_enable = one_config.getAttribute('enable')
-                    config_path = one_config.getAttribute('path')
                     config_vmname = one_config.getAttribute('vmname')
                     config_vmid = one_config.getAttribute('vmid')
                     config_start_cmd = one_config.getAttribute('startCommand')
@@ -76,7 +75,6 @@ class VMSModifyHandler(object):
                         'vmid': config_vmid,
                         'vmname': config_vmname,
                         'enable': config_enable,
-                        'path': config_path,
                         'startCommand': config_start_cmd,
                         'appiumCommand': config_appium_cmd,
                     })
@@ -169,80 +167,3 @@ class VMSModifyHandler(object):
             return True
 
         return False
-
-    @staticmethod
-    def rebuild(vms_file=''):
-
-        if not os.path.isfile(vms_file):
-            print("Can't location the vms_file = ", vms_file)
-            return
-
-        print("Now rebuilding ...")
-
-        # 获取文件所在路径
-        vmsDirectory = os.path.dirname(vms_file)
-        vmsFileName = os.path.basename(vms_file)
-
-        # 将获得的xml文件送入到dom来解析
-        dom = xml.dom.minidom.parse(vms_file)
-        root = dom.documentElement  # 根节点 为 <MemuHyperv xmlns="http://www.memuhyperv.org/" version="1.12-windows">
-
-        Machine = root.getElementsByTagName('Machine')[0]
-
-        # 获得Hardware的配置
-        Hardware = Machine.getElementsByTagName('Hardware')[0]
-
-        # 获取Hardware > Network
-        Network = Hardware.getElementsByTagName('Network')[0]
-
-        # 修改MAC地址
-        Network_Adapters = Network.getElementsByTagName('Adapter')
-        for adapter in Network_Adapters:
-            if adapter.hasAttribute('enabled') and adapter.hasAttribute('MACAddress'):
-                enabled = adapter.getAttribute('enabled')
-                if enabled:
-                    mac_address = "".join(Utils.generate_new_mac_address_list()).upper()
-                    adapter.setAttribute('MACAddress', mac_address)
-                    print("[%s] = %s" % ('MACAddress', mac_address))
-
-        # 获取 GuestProperties
-        GuestProperties = Hardware.getElementsByTagName('GuestProperties')[0].getElementsByTagName('GuestProperty')
-
-        # 定义需要更新的内容
-        new_gps_pos = Utils.generate_new_pos()
-        win_update_map = {
-            'bssid': Utils.generate_new_bssid().upper(),
-            'cellid': Utils.generate_new_cellid(),
-            'imei': Utils.generate_new_imei(),
-            'linenum': Utils.generate_new_phone_number(),
-            'latitude': new_gps_pos['latitude'],
-            'longitude': new_gps_pos['longitude'],
-            'ssid': Utils.generate_new_wifi_id(),
-            'simserial': Utils.generate_new_simserial(),
-        }
-
-        # 循环查找，开始更新
-        for one_GuestProperty in GuestProperties:
-            if one_GuestProperty.hasAttribute('name') and one_GuestProperty.hasAttribute('value'):
-                name = one_GuestProperty.getAttribute('name')
-
-                # 根据name的不同，来更新value
-                if name in win_update_map:
-                    one_GuestProperty.setAttribute('value', win_update_map[name])
-                    print("[%s] = %s" % (name, win_update_map[name]))
-
-        # SharedFolders 需要重新整理为公共可用值
-        # 由于SharedFolder 默认是不挂载的。autoMount = false, 所以暂时不启动
-
-        # 尝试源路径存储
-        save_vms_config_files = [
-            os.path.join(vmsDirectory, vmsFileName),
-            os.path.join(vmsDirectory, '{}-prev'.format(vmsFileName))
-        ]
-        for vms_file in save_vms_config_files:
-            with open(file=vms_file, mode='w', encoding='utf-8') as fh:
-                try:
-                    dom.writexml(fh)
-                    print('The new contents are saved, the file path = ', vms_file)
-                except Exception as err:
-                    print('Save file Error:', err)
