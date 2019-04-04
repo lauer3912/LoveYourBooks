9c2f7924db213a6a33a8f6f3a7390bcce0fe73cf
Ian sun <lauer3912@gmail.com>
2019-04-05 07:08:21 +0800

    优化：添加快速浏览模式，防止广告点击过快进行

--- Python-Books/code-projects/auto-appium-android/run.py
+++ Python-Books/code-projects/auto-appium-android/run.py
@@ -418,6 +418,7 @@ def starup(want_open_url):
         # 快速浏览模式，将降低很多指标参数，不点击广告等等
         enable_quick_browser_mode = round(random.uniform(0.2, 10), 2) <= 5
         if enable_quick_browser_mode:
+            logger.info("开启快速浏览模式 ....")
             max_page_load_timeout = random.randint(60, 90)
             max_script_timeout = 30
 
