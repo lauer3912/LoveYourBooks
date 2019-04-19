94edc106070b239f0c312f9e37523ac89bd98910
lauer3912 <lauer3912@gmail.com>
2019-04-20 07:05:06 +0800

    log size to 100MB

--- Python-Books/code-projects/auto-appium-android/win_automation.py
+++ Python-Books/code-projects/auto-appium-android/win_automation.py
@@ -22,7 +22,7 @@ if os.path.isfile(log_file_path):
         os.remove(log_file_path)
     except:
         pass
-handler = logging.handlers.RotatingFileHandler(log_file_path, maxBytes=1024 * 1024 * 50,
+handler = logging.handlers.RotatingFileHandler(log_file_path, maxBytes=1024 * 1024 * 100,
                                                backupCount=1)  # 实例化 handler
 fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
 formatter = logging.Formatter(fmt)  # 实例化 formatter
